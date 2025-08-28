bfc2965a09937c80c49aaf42c95e29da3e65b0c9
ale94 <alejandrorua111@gmail.com>
2025-08-28 17:41:25 -0300

    refactor: modify atributte user request

--- src/main/java/com/ale94/digital_banking_api/api/models/requests/UserRequest.java
+++ src/main/java/com/ale94/digital_banking_api/api/models/requests/UserRequest.java
@@ -15,7 +15,7 @@ public class UserRequest implements Serializable {
     private String name;
     private String email;
     private String phone;
-    private String identityDocument;
+    private String identityDoc;
     private String username;
     private String password;
 }
