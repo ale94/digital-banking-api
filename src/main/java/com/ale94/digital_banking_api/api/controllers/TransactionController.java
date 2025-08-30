f5dba63181c6746940015976c7941c722a86d658
ale94 <alejandrorua111@gmail.com>
2025-08-29 23:20:05 -0300

    feat: add download pdf controller

--- src/main/java/com/ale94/digital_banking_api/api/controllers/TransactionController.java
+++ src/main/java/com/ale94/digital_banking_api/api/controllers/TransactionController.java
@@ -2,6 +2,8 @@ package com.ale94.digital_banking_api.api.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,16 @@ public class TransactionController {
     @GetMapping(path = "/{accountNumber}")
     public ResponseEntity<List<TransactionResponse>> getAllTransactions(@PathVariable String accountNumber) {
         return ResponseEntity.ok(this.transactionService.getAllTransactions(accountNumber));
+    }
+
+    @GetMapping(path = "download-pdf/{operationNumber}")
+    public ResponseEntity<byte[]> downloadPdfReceipt(@PathVariable String operationNumber) {
+        byte[] pdfBytes = this.transactionService.generatePDF(operationNumber);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=receipt_" + operationNumber + ".pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdfBytes);
     }
 
 }
