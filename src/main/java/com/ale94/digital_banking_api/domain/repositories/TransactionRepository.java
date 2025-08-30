d0a9fc353d60d44bc9005d926c2448d8ec232a2c
ale94 <alejandrorua111@gmail.com>
2025-08-29 23:18:50 -0300

    feat: add find by operation number method

--- src/main/java/com/ale94/digital_banking_api/domain/repositories/TransactionRepository.java
+++ src/main/java/com/ale94/digital_banking_api/domain/repositories/TransactionRepository.java
@@ -1,7 +1,12 @@
 package com.ale94.digital_banking_api.domain.repositories;
 
-import com.ale94.digital_banking_api.domain.entities.TransactionEntity;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ale94.digital_banking_api.domain.entities.TransactionEntity;
+
 public interface TransactionRepository extends JpaRepository<TransactionEntity, String> {
+    
+    Optional<TransactionEntity> findByOperationNumber(String operationNumber);
 }
