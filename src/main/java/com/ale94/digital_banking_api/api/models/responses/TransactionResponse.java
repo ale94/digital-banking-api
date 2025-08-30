89fbe8a42eca47b743f14a709534535233484101
ale94 <alejandrorua111@gmail.com>
2025-08-29 21:47:49 -0300

    feat: add transaction response

---
+++ src/main/java/com/ale94/digital_banking_api/api/models/responses/TransactionResponse.java
@@ -0,0 +1,26 @@
+package com.ale94.digital_banking_api.api.models.responses;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class TransactionResponse implements Serializable {
+    private String operationNumber;
+    private String destinationAccount;
+    private BigDecimal amount;
+    private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss")
+    private LocalDateTime date;
+    private String notification;
+}
