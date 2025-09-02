fa9fe17505730c58573aee1645b10ca4e1d81719
ale94 <alejandrorua111@gmail.com>
2025-09-01 21:46:47 -0300

    feat: add bad request controller advice

---
+++ src/main/java/com/ale94/digital_banking_api/api/controllers/error_handler/BadRequestController.java
@@ -0,0 +1,25 @@
+package com.ale94.digital_banking_api.api.controllers.error_handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ale94.digital_banking_api.api.models.responses.BaseErrorResponse;
+import com.ale94.digital_banking_api.api.models.responses.ErrorResponse;
+import com.ale94.digital_banking_api.util.exceptions.IdNotFoundException;
+
+@RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestController {
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public BaseErrorResponse handleIdNotFoundException(IdNotFoundException exception) {
+        return ErrorResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+    }
+
+}
