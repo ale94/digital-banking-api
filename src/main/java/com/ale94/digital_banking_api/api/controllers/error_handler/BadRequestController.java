acc7bd50791dccd1a04bfad9229a666c97d7fcd6
ale94 <alejandrorua111@gmail.com>
2025-09-02 23:06:39 -0300

    refactor: refactor bad request controller

--- src/main/java/com/ale94/digital_banking_api/api/controllers/error_handler/BadRequestController.java
+++ src/main/java/com/ale94/digital_banking_api/api/controllers/error_handler/BadRequestController.java
@@ -1,12 +1,16 @@
 package com.ale94.digital_banking_api.api.controllers.error_handler;
 
+import java.util.ArrayList;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.ale94.digital_banking_api.api.models.responses.BaseErrorResponse;
 import com.ale94.digital_banking_api.api.models.responses.ErrorResponse;
+import com.ale94.digital_banking_api.api.models.responses.ErrorsResponse;
 import com.ale94.digital_banking_api.util.exceptions.IdNotFoundException;
 
 @RestControllerAdvice
@@ -16,7 +20,18 @@ public class BadRequestController {
     @ExceptionHandler(IdNotFoundException.class)
     public BaseErrorResponse handleIdNotFoundException(IdNotFoundException exception) {
         return ErrorResponse.builder()
-                .message(exception.getMessage())
+                .error(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        var errors = new ArrayList<String>();
+        exception.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+        return ErrorsResponse.builder()
+                .errors(errors)
                 .status(HttpStatus.BAD_REQUEST.name())
                 .code(HttpStatus.BAD_REQUEST.value())
                 .build();
