ab8d5762781d70c78882c265acd4838218b1b5a8
ale94 <alejandrorua111@gmail.com>
2025-09-02 23:05:17 -0300

    feat: add validations to the user request

--- src/main/java/com/ale94/digital_banking_api/api/models/requests/UserEditRequest.java
+++ src/main/java/com/ale94/digital_banking_api/api/models/requests/UserEditRequest.java
@@ -2,6 +2,9 @@ package com.ale94.digital_banking_api.api.models.requests;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +16,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserEditRequest implements Serializable {
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
+    @Size(min = 10, max = 12, message = "The size must be between 10 and 12 characters long")
+    @NotBlank(message = "The phone is required")
     private String phone;
 
 }
--- src/main/java/com/ale94/digital_banking_api/api/models/requests/UserRequest.java
+++ src/main/java/com/ale94/digital_banking_api/api/models/requests/UserRequest.java
@@ -7,15 +7,32 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
 public class UserRequest implements Serializable {
+
+    @Size(min = 6, max = 12, message = "The size must be between 6 and 12 characters long")
+    @NotBlank(message = "The name is required")
     private String name;
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
+    @Size(min = 10, max = 12, message = "The size must be between 10 and 12 characters long")
+    @NotBlank(message = "The phone is required")
     private String phone;
+    @Size(min = 8, max = 10, message = "The size must be between 8 and 10 characters long")
+    @NotBlank(message = "The identityDoc is required")
     private String identityDoc;
+    @Size(min = 6, max = 12, message = "The size must be between 6 and 12 characters long")
+    @NotBlank(message = "The username is required")
     private String username;
+    @Size(min = 8, max = 12, message = "The size must be between 8 and 12 characters long")
+    @NotBlank(message = "The password is required")
     private String password;
 }
