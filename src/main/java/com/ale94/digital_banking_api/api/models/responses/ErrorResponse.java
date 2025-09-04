010b8676cd8d2d254e3786bf8aef29b2398c1007
ale94 <alejandrorua111@gmail.com>
2025-09-04 18:31:12 -0300

    refactor: refactor add equals and hascode

--- src/main/java/com/ale94/digital_banking_api/api/models/responses/ErrorResponse.java
+++ src/main/java/com/ale94/digital_banking_api/api/models/responses/ErrorResponse.java
@@ -2,9 +2,11 @@ package com.ale94.digital_banking_api.api.models.responses;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
--- src/main/java/com/ale94/digital_banking_api/api/models/responses/ErrorsResponse.java
+++ src/main/java/com/ale94/digital_banking_api/api/models/responses/ErrorsResponse.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
