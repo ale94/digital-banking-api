2ef8fa746dc68abd9d892c7296b2e174cce593fa
ale94 <alejandrorua111@gmail.com>
2025-09-05 19:17:47 -0300

    refactor: refactor create schema sql

--- db/sql/create_schema.sql
+++ db/sql/create_schema.sql
@@ -9,6 +9,17 @@ CREATE TABLE users (
     reg_date TIMESTAMP(6),
     username VARCHAR(255)
 );
+CREATE TABLE roles (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE users_roles (
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
 CREATE TABLE account (
     account_number VARCHAR(255) PRIMARY KEY,
     alias VARCHAR(255),
