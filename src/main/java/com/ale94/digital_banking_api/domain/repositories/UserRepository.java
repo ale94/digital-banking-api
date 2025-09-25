410b7691206be59a5385253f79b1511902051658
ale94 <alejandrorua111@gmail.com>
2025-09-24 23:04:22 -0300

    feat: add find by username method

--- src/main/java/com/ale94/digital_banking_api/domain/repositories/UserRepository.java
+++ src/main/java/com/ale94/digital_banking_api/domain/repositories/UserRepository.java
@@ -1,7 +1,13 @@
 package com.ale94.digital_banking_api.domain.repositories;
 
 import com.ale94.digital_banking_api.domain.entities.UserEntity;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+    
 }
