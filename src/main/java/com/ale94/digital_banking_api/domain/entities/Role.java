b53cc9c5354a17711bbbb0e726a150bafab90411
ale94 <alejandrorua111@gmail.com>
2025-09-24 23:03:01 -0300

    feat: add role entity

---
+++ src/main/java/com/ale94/digital_banking_api/domain/entities/Role.java
@@ -0,0 +1,23 @@
+package com.ale94.digital_banking_api.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
