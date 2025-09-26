8e846325dd912b9a64fcdabcac7b8926a346a30e
ale94 <alejandrorua111@gmail.com>
2025-09-25 23:26:34 -0300

    feat: add filter chain method

---
+++ src/main/java/com/ale94/digital_banking_api/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ale94.digital_banking_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                auth -> auth.requestMatchers(HttpMethod.GET, "/api/users/**").permitAll().anyRequest().authenticated()
+
+        )
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
