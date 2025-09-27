ee863492ae5db73acd071ba58151b302be12e72a
ale94 <alejandrorua111@gmail.com>
2025-09-27 00:13:12 -0300

    feat: add security config

--- src/main/java/com/ale94/digital_banking_api/config/SecurityConfig.java
+++ src/main/java/com/ale94/digital_banking_api/config/SecurityConfig.java
@@ -3,22 +3,59 @@ package com.ale94.digital_banking_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.ale94.digital_banking_api.infraestructure.services.UserDetailsServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(
-                auth -> auth.requestMatchers(HttpMethod.GET, "/api/users/**").permitAll().anyRequest().authenticated()
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtils jwtUtils;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
-        )
+    // Cadena de filtro de seguridad - comportamiento de acceso
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils);
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+
+        return http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/users/**").permitAll()
+                .anyRequest()
+                .authenticated())
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    // Encriptacion necesaria para spring security
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // administrador de autenticacion de usuario de spring security
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder)
+                .and().build();
     }
 
 }
