c79494572c94a83d662ef021e82425f103215c4c
ale94 <alejandrorua111@gmail.com>
2025-09-27 00:13:41 -0300

    feat: add authorization filter

---
+++ src/main/java/com/ale94/digital_banking_api/config/JwtAuthorizationFilter.java
@@ -0,0 +1,48 @@
+package com.ale94.digital_banking_api.config;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ale94.digital_banking_api.infraestructure.services.UserDetailsServiceImpl;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenHeader = request.getHeader("Authorization");
+
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+
+            String token = tokenHeader.substring(7);
+
+            if (jwtUtils.isTokenValid(token)) {
+
+                String username = jwtUtils.getUsernameFromToken(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        username, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
